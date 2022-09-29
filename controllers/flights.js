5b455ea06b03e5de027f2c0aa78eca63778d5e9c
GFou <gfouchard20@outlook.com>
2022-09-28 20:07:16 -0400

    make ticket/price show on view/show.ejs

--- controllers/flights.js
+++ controllers/flights.js
@@ -89,7 +89,22 @@ function update(req, res) {
 }
 
 function createTicket(req, res) {
-
+  Flight.findById(req.params.id)
+  .then(flight => {
+    flight.tickets.push(req.body)
+    flight.save()
+    .then(() => {
+      res.redirect(`/flights/${flight._id}`)
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/')
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
 }
 
 export {
